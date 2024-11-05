705d0d2e0f37958042346bdd4e051786aba0d208
albin <albin.tardivel@insa-rennes.fr>
2024-11-05 10:17:07 +0700

    View a page from a text file

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Write and Read in a document
 
 ![plot](./screen_appli/writeRead.png)
+
+## Web Server
+
+![plot](./screen_appli/webServer.png)
+
+## View a page from a text file
+
+![plot](./screen_appli/viewPage.png)
--- wiki.go
+++ wiki.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -31,11 +33,23 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+/*
+	First, this function extracts the page title from r.URL.Path, the path component of the request URL. 
+	The Path is re-sliced with [len("/view/"):] to drop the leading "/view/" component of the request path. 
+	This is because the path will invariably begin with "/view/", which is not part of the page's title.
+	The function then loads the page data, formats the page with a string of simple HTML, and writes it to w, the http.ResponseWriter. 
+*/
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
-
+func main() {
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	// p1.save()
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler) // Move to http://localhost:8080/view/test (where test is the name of the file we want to print)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
