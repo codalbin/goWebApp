7d6d6f964191760aaf7cabe8303c35ccbd99487c
albin <albin.tardivel@insa-rennes.fr>
2024-11-05 10:30:07 +0700

    create Editing page to modify, save and display file

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 ## View a page from a text file
 
 ![plot](./screen_appli/viewPage.png)
+
+## Add editing page 
+
+![plot](./screen_appli/editPage.png)
--- wiki.go
+++ wiki.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -33,16 +34,74 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, err := template.ParseFiles(tmpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 /*
-	First, this function extracts the page title from r.URL.Path, the path component of the request URL. 
-	The Path is re-sliced with [len("/view/"):] to drop the leading "/view/" component of the request path. 
-	This is because the path will invariably begin with "/view/", which is not part of the page's title.
-	The function then loads the page data, formats the page with a string of simple HTML, and writes it to w, the http.ResponseWriter. 
+First, this function extracts the page title from r.URL.Path, the path component of the request URL.
+The Path is re-sliced with [len("/view/"):] to drop the leading "/view/" component of the request path.
+This is because the path will invariably begin with "/view/", which is not part of the page's title.
+The function then loads the page data, formats the page with a string of simple HTML, and writes it to w, the http.ResponseWriter.
 */
+// func viewHandler(w http.ResponseWriter, r *http.Request) {
+// 	title := r.URL.Path[len("/view/"):]
+// 	p, _ := loadPage(title)
+// 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+// }
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+// Function to load the page (or, if it doesn't exist, create an empty Page struct), and displays an HTML form
+// Works but not a good way to print html like this => better to use html file
+
+//	func editHandler(w http.ResponseWriter, r *http.Request) {
+//		title := r.URL.Path[len("/edit/"):]
+//		p, err := loadPage(title)
+//		if err != nil {
+//			p = &Page{Title: title}
+//		}
+//		fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+//			"<form action=\"/save/%s\" method=\"POST\">"+
+//			"<textarea name=\"body\">%s</textarea><br>"+
+//			"<input type=\"submit\" value=\"Save\">"+
+//			"</form>",
+//			p.Title, p.Title, p.Body)
+//	}
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
@@ -51,5 +110,7 @@ func main() {
 	// p2, _ := loadPage("TestPage")
 	// fmt.Println(string(p2.Body))
 	http.HandleFunc("/view/", viewHandler) // Move to http://localhost:8080/view/test (where test is the name of the file we want to print)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
